0c866a1431af9d49ecbf58f7eaf8655decee6521
杨睿龙 <ReadLamp@163.com>
2023-05-01 15:53:44 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -97,7 +97,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>ChatGPT ReadLamp</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          🦄 Welcome to ChatGPT.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
